19493594998915c664b29a6173cd6e9080c75209
GirtsFreimanis <girtslp@gmail.com>
2023-11-10 19:43:37 +0200

    big changes made

--- app/Api/BinanceAPI.php
+++ app/Api/BinanceAPI.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace App\Api;
 
 use App\CryptoPair;
+use GuzzleHttp\Client;
 
 class BinanceAPI
 {
+    private Client $client;
     const API_URL = "https://api4.binance.com/api/v3/ticker/24hr?symbol=";
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'verify' => false
+        ]);
+    }
 
     public function getTickerData(CryptoPair $cryptoPair): array
     {
         $apiUrl = self::API_URL . $cryptoPair->getSymbol();
-        $response = file_get_contents($apiUrl);
+        $response = $this->client->get($apiUrl);
 
-        if (!$response) {
-            exit ("Failed to make API request for {$cryptoPair->getSymbol()}\n");
-        }
-        return json_decode($response, true);
+        return json_decode((string)$response->getBody(), true);
     }
 }
